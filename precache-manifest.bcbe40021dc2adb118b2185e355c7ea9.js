6b195077f2716707895f221395276ea70c7701a1
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-13 19:17:47 -0300

    Updates

---
+++ precache-manifest.bcbe40021dc2adb118b2185e355c7ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f28b9bb482f75303f389c3aed70666",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "f5eb2f4eab3f9280efab",
+    "url": "/starwars-planets/static/css/main.33422f8a.chunk.css"
+  },
+  {
+    "revision": "e0ba45bab119ab44864a",
+    "url": "/starwars-planets/static/js/2.63c9e594.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars-planets/static/js/2.63c9e594.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5eb2f4eab3f9280efab",
+    "url": "/starwars-planets/static/js/main.5c0d2437.chunk.js"
+  },
+  {
+    "revision": "59624a367c47ed4f5c0e",
+    "url": "/starwars-planets/static/js/runtime-main.0f4f4c19.js"
+  }
+]);
